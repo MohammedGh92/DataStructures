e5c934764a7b399cd9bc72af291c7a17e9d30bbb
Mohammed W. Ghabayen <mohammedghabyen@gmail.com>
2022-07-31 14:50:15 +0300

    Updates
    -Heap

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using DataStructures.Graphs;
 using DataStructures.Graphs.TopSort;
+using DataStructures.Utils;
 
 namespace DataStructures
 {
@@ -8,8 +9,11 @@ namespace DataStructures
     {
         public static void Main(String[] args)
         {
-            AverageOfLevels averageOfLevels = new AverageOfLevels();
-            averageOfLevels.GetAverageOfLevels();
+
+            Heap heap = new Heap(new int[] { 1, 2, 3, 4, 5, 6, 7, 8 });
+            heap.build();
+            heap.sort();
+            heap.printArray();
         }
     }
 }
