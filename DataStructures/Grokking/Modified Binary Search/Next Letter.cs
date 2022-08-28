f0b2a6c95592ba97e442d4c96bb94ea8448a76d0
Mohammed W. Ghabayen <mohammedghabyen@gmail.com>
2022-08-28 18:44:24 +0300

    Revert "Revert "Update:""
    This reverts commit fb351d2bd7f2fabea9429d8adc245e969a96a451.

---
+++ DataStructures/Grokking/Modified Binary Search/Next Letter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+namespace DataStructures.Grokking.ModifiedBinarySearch
+{
+    public class Next_Letter
+    {
+        char[] letters;
+        char key;
+        public Next_Letter()
+        {
+            letters = new char[] { 'a', 'c', 'f', 'h' };
+            key = 'g';
+        }
+
+        public char searchNextLetter()
+        {
+
+            int left = 0;
+            int right = letters.Length - 1;
+
+            while (left < right)
+            {
+
+                int mid = (left + right) / 2;
+
+                if (letters[mid] == key)
+                    return letters[mid];
+                else if (letters[mid] < key)
+                    left = mid + 1;
+                else
+                    right = mid - 1;
+            }
+
+            return letters[left];
+        }
+    }
+}
---
+++ DataStructures/Grokking/Modified Binary Search/Rotation Count.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace DataStructures.Grokking.ModifiedBinarySearch
+{
+    public class Rotation_Count
+    {
+        int[] nums;
+        public Rotation_Count()
+        {
+            nums = new int[] { 1, 3, 8, 10 };
+        }
+
+        public int countRotations()
+        {
+            return GetRotationIndx();
+        }
+
+        private int GetRotationIndx()
+        {
+            int left = 0;
+            int right = nums.Length - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (nums[mid] > nums[right])
+                    left = mid + 1;
+                else if (nums[mid] > nums[left])
+                    right = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
